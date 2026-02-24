1d53d9b6a45691ef65a8e9e3e96a9a38862724c9
Mehedi Mostafa Hafiz <mehedimostafabisd@gmail.com>
2026-02-24 23:35:22 +0000

    a

--- Arrays_and_Hashing/two_sum.py
+++ Arrays_and_Hashing/two_sum.py
@@ -7,3 +7,15 @@ def twoSum(self, nums: List[int], target: int) -> List[int]:
         return []
 # Time complexity: O(n^2)
 # Space complexity: O(1)
+
+# 2: Hash Table
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        prevMap = {} #val : index
+
+        for i, n in enumerate(nums):
+            diff = target - n
+            if diff in prevMap:
+                return [prevMap[diff], i]
+            prevMap[n] = i
+        return
