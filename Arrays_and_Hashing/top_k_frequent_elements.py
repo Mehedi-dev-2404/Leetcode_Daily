cc25c23bb86a3e48934776008f90a9ab231cb844
Mehedi Mostafa Hafiz <mehedimostafabisd@gmail.com>
2026-03-08 23:18:10 +0000

    Update top_k_frequent_elements.py

--- Arrays_and_Hashing/top_k_frequent_elements.py
+++ Arrays_and_Hashing/top_k_frequent_elements.py
@@ -11,3 +11,22 @@ def topKFrequent(self, nums: List[int], k: int) -> List[int]:
     return [sorted_items[i][0] for i in range(k)]
 # Time complexity: O(n log n) due to sorting the count dictionary.
 # Space complexity: O(n) for storing the count of each unique number in the dictionary.
+
+def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        count = {}
+        freq = [[] for i in range(len(nums) + 1)]
+
+        for n in nums:
+            count[n] = 1 + count.get(n, 0)
+        for n, c in count.items():
+            freq[c].append(n)
+
+        res = []
+        for i in range(len(freq) - 1, 0, -1):
+            for n in freq[i]:
+                res.append(n)
+                if len(res) == k:
+                    return res
+                
+# Time complexity: O(n) for counting the frequency and O(n) for iterating through the frequency list, resulting in O(n) overall.
+# Space complexity: O(n) for the count dictionary and O(n) for the frequency list
